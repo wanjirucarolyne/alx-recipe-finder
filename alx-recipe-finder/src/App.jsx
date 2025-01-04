fe36938cdfbc51e0f6d52066587748da3f205fc3
carolyne wanjiru <carolynekamau097@gmail.com>
2025-01-04 14:24:06 +0300

    Initial commit

---
+++ alx-recipe-finder/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import './App.css'
+import SearchBar from "./components/SearchBar"
+import RecipeCard from "./components/RecipeCard"
+
+const apiUrl ="https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+function App() {
+
+  const[isLoading, setisLoading]=useState(false);
+  const [query, setQuery]=useState("");
+  const[recipes, setRecipes]=useState([]);
+
+//fetching data from the API
+
+const searchRecipes = async() => {
+
+  setisLoading(true);
+  const url =apiUrl + query;
+  const res =await fetch(url);
+  const data =await res.json();
+  //console.log(data);
+  setRecipes(data.meals);
+  setisLoading(false);
+
+
+};
+
+useEffect(()=> {
+
+  searchRecipes()
+
+}, []);
+  return (
+    
+      <div className='container'>
+        <h2>DELIGHT RECIPES</h2>
+        <div classname="recipes">
+
+          {recipes? recipes.map((recipe => ))}
+
+        </div>
+        </div>
+    
+  );
+}
+
+export default App;
